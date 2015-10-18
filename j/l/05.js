c5fe2b6d163dbe306c5041200af6f94c39fe7c5a
edfcez <edfcez@yandex.com>
2015-10-18 19:55:03 +0300

    up js

---
+++ j/l/05.js
@@ -0,0 +1,196 @@
+var reformalOptions = {
+    project_id: 914098,
+    project_host: "wiakia.reformal.ru",
+    tab_orientation: "right",
+    tab_indent: "50%",
+    tab_bg_color: "#777",
+    tab_border_color: "#FFFFFF",
+    tab_image_url: "//farm1.staticflickr.com/600/20799754844_1cbd67d4d6_o.png",
+    tab_border_width: 2
+};
+function preloader() {
+    if (document.images) {
+
+        if (window.screen.availWidth > 480) {
+            (function (w, d, c) {
+                var s = d.createElement('script'), h = d.getElementsByTagName('script')[0], e = d.documentElement;
+                if ((' ' + e.className + ' ').indexOf(' ya-page_js_yes ') === -1) {
+                    e.className += ' ya-page_js_yes';
+                }
+                s.type = 'text/javascript';
+                s.async = true;
+                s.charset = 'utf-8';
+                s.src = (d.location.protocol === 'https:' ? 'https:' : 'http:') + '//site.yandex.net/v2.0/js/all.js';
+                h.parentNode.insertBefore(s, h);
+                (w[c] || (w[c] = [])).push(function () {
+                    Ya.Site.Form.init()
+                })
+            })(window, document, 'yandex_site_callbacks');
+            (function () {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.async = true;
+                script.src = '//s.wiakia.com/j/reformal.js';
+                document.getElementsByTagName('head')[0].appendChild(script);
+            })();
+        }
+    }
+
+
+}
+function preCouImg() {
+    // Добовляем изображения флага для характеристики
+    var cTai = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA5MDAgNjAwIj4NCjxwYXRoIGZpbGw9IiNkZTEwMTgiIGQ9Im0wLDBoOTAwdjYwMGgtOTAweiIvPg0KPHBhdGggZmlsbD0iI2ZmZiIgZD0ibTAsMTAwaDkwMHY0MDBoLTkwMHoiLz4NCjxwYXRoIGZpbGw9IiMwMDI0N2QiIGQ9Im0wLDIwMGg5MDB2MjAwaC05MDB6Ii8+DQo8L3N2Zz4NCg==';
+    var cFr = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA5MDAgNjAwIj4NCjxwYXRoIGZpbGw9IiNlZDI5MzkiIGQ9Im0wLDBoOTAwdjYwMGgtOTAweiIvPg0KPHBhdGggZmlsbD0iI2ZmZiIgZD0ibTAsMGg2MDB2NjAwaC02MDB6Ii8+DQo8cGF0aCBmaWxsPSIjMDAyMzk1IiBkPSJtMCwwaDMwMHY2MDBoLTMwMHoiLz4NCjwvc3ZnPg0K';
+    var cImg = ['<img class="ic" alt="Таиланд" src="'+cTai+'" /><span class="instok">Таиланд</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>','<img class="ic" alt="Франция" src="'+cFr+'"/><span class="instok">Франция</span>'];
+
+    for (var q = 0; q < cImg.length; q++) {
+        document.getElementById('c' + q).innerHTML = cImg[q];
+    }
+}
+
+function addLink(event) {
+    event.preventDefault();
+
+    var pagelink = '<div style="position: absolute; bottom: 0px; left: -9999px;"><a href="' + document.location.href + '">источник</a></div>',
+        copytext = window.getSelection() + pagelink;
+
+    (event.clipboardData || window.clipboardData).setData('Text', copytext);
+}
+document.addEventListener('copy', addLink);
+var toggle = document.getElementsByClassName('navbar-toggle')[0], collapse = document.getElementsByClassName('navbar-collapse')[0], dropdowns = document.getElementsByClassName('dropdown');
+;
+function toggleMenu() {
+    collapse.classList.toggle('collapse');
+    collapse.classList.toggle('in');
+}
+function closeMenus() {
+    for (var j = 0; j < dropdowns.length; j++) {
+        dropdowns[j].getElementsByClassName('dropdown-toggle')[0].classList.remove('dropdown-open');
+        dropdowns[j].classList.remove('open');
+    }
+}
+for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].addEventListener('click', function () {
+        if (document.body.clientWidth < 768) {
+            var open = this.classList.contains('open');
+            closeMenus();
+            if (!open) {
+                this.getElementsByClassName('dropdown-toggle')[0].classList.toggle('dropdown-open');
+                this.classList.toggle('open');
+            }
+        }
+    });
+}
+function closeMenusOnResize() {
+    if (document.body.clientWidth >= 768) {
+        closeMenus();
+        collapse.classList.add('collapse');
+        collapse.classList.remove('in');
+    }
+}
+window.addEventListener('resize', closeMenusOnResize, false);
+toggle.addEventListener('click', toggleMenu, false);
+var e = document.getElementsByTagName('div');
+for (var k = 0; k < e.length; k++) {
+    if (e[k].className.indexOf('share42init') != -1) {
+        if (e[k].getAttribute('data-url') != -1) var u = e[k].getAttribute('data-url');
+        if (e[k].getAttribute('data-title') != -1) var t = e[k].getAttribute('data-title');
+        if (e[k].getAttribute('data-image') != -1) var i = e[k].getAttribute('data-image');
+        if (e[k].getAttribute('data-description') != -1) var d = e[k].getAttribute('data-description');
+        if (e[k].getAttribute('data-icons-file') != -1) var fn = e[k].getAttribute('data-icons-file');
+        if (!u) u = location.href;
+        if (!t) t = document.title;
+        function desc() {
+            var meta = document.getElementsByTagName('meta');
+
+            for (var m = 0; m < meta.length; m++) {
+                if (meta[m].name.toLowerCase() == 'description') {
+                    return meta[m].content;
+                }
+            }
+            return '';
+        }
+
+        if (!d) d = desc();
+        u = encodeURIComponent(u);
+        t = encodeURIComponent(t);
+        t = t.replace(/\'/g, '%27');
+        i = encodeURIComponent(i);
+        d = encodeURIComponent(d);
+        d = d.replace(/\'/g, '%27');
+        var fbQuery = 'u=' + u;
+        if (i != 'null' && i != '') fbQuery = 's=100&p[url]=' + u + '&p[title]=' + t + '&p[summary]=' + d + '&p[images][0]=' + i;
+        var vkImage = '';
+        if (i != 'null' && i != '') vkImage = '&image=' + i;
+        var s = new Array('"" onclick="return fav(this);" title="Сохранить в избранное браузера"', '"#" data-count="twi" onclick="window.open(\'https://twitter.com/intent/tweet?text=' + t + '&url=' + u + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Добавить в Twitter"', '"#" data-count="mail" onclick="window.open(\'http://connect.mail.ru/share?url=' + u + '&title=' + t + '&description=' + d + '&imageurl=' + i + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться в Моем Мире@Mail.Ru"', '"http://www.livejournal.com/update.bml?event=' + u + '&subject=' + t + '" title="Опубликовать в LiveJournal"', '"http://share.yandex.ru/go.xml?service=moikrug&url=' + u + '&title=' + t + '&description=' + d + '" title="Поделиться в Мой Круг"', '"#" data-count="vk" onclick="window.open(\'http://vk.com/share.php?url=' + u + '&title=' + t + vkImage + '&description=' + d + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться В Контакте"', '"#" data-count="fb" onclick="window.open(\'http://www.facebook.com/sharer.php?m2w&' + fbQuery + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться в Facebook"', '"#" data-count="gplus" onclick="window.open(\'https://plus.google.com/share?url=' + u + '\', \'_blank\', \'scrollbars=0, resizable=1, menubar=0, left=100, top=100, width=550, height=440, toolbar=0, status=0\');return false" title="Поделиться в Google+"');
+        var l = '';
+        for (j = 0; j < s.length; j++) l += '<a rel="nofollow" style="display:inline-block;vertical-align:bottom;width:32px;height:32px;margin:0 6px 6px 0;padding:0;outline:none;background:url(' + '//farm6.staticflickr.com/5639/21764518630_bb5c1cbacc_o.png' + ') -' + 32 * j + 'px 0 no-repeat" href=' + s[j] + ' target="_blank"></a>';
+        e[k].innerHTML = '<span id="share42">' + l + '</span>';
+    }
+}
+
+function fav(a) {
+    var title = document.title;
+    var url = document.location;
+    try {
+        window.external.AddFavorite(url, title);
+    } catch (e) {
+        try {
+            window.sidebar.addPanel(title, url, '');
+        } catch (e) {
+            if (typeof (opera) == 'object' || window.sidebar) {
+                a.rel = 'sidebar';
+                a.title = title;
+                a.url = url;
+                a.href = url;
+                return true;
+            }
+            else {
+                alert('Нажмите Ctrl-D, чтобы добавить страницу в закладки');
+            }
+        }
+    }
+    return false;
+}
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+addLoadEvent(preCouImg);
+addLoadEvent(preloader);
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function () {
+        try {
+            w.yaCounter32028596 = new Ya.Metrika({
+                id: 32028596,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                webvisor: true
+            });
+        } catch (e) {
+        }
+    });
+    var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () {
+        n.parentNode.insertBefore(s, n);
+    };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/watch.js";
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else {
+        f();
+    }
+})(document, window, "yandex_metrika_callbacks");
